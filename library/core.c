f74bbdf7622f31062cbbb7533d26adfa986aa174
Mirko Vogt <dev@nanl.de>
2010-11-23 15:35:59 +0100

    add some structures visualizing the idea of the library design... does compile and work for hardcoded data

--- library/core.c
+++ library/core.c
@@ -11,20 +11,95 @@ struct packet {
     char data[DATA_MAX]; // payload
 };
 
-struct packet foo = {
-    .duration = 2,
-    .count = 2,
-    .data = "010",
+enum type {
+    SOCKET_TYPE_2272,
+    SOCKET_TYPE_FNORD,
 };
 
-int main() {
-    printf("data: %s\n", foo.data);
+struct device {
+    int type;
+    char *id;
+    char *name;
+    char *category;
+    struct packet (*on)(char system_code, char unit_code);
+    struct packet (*off)(char system_code, char unit_code);
+    union {
+        struct {
+            char system_code;
+            char unit_code;
+        } device_2272;
+        struct {
+            char ipaddr;
+            char port;
+        } device_fnord;
+    };
+};
+
+int write(struct packet *_packet) {
+    printf("data: %s\n", _packet->data);
     FILE *fd = fopen(DEVICE_NAME, "w");
-//    foo.data = "01";
     if (fd == NULL) {
         printf("can not open device: %s\n", DEVICE_NAME);
         return EXIT_FAILURE;
     }
-    fwrite(&foo, 1, sizeof(foo), fd);
+    if (fwrite(_packet, 1, sizeof(_packet), fd) != sizeof(_packet))
+        return EXIT_FAILURE;
     fclose(fd);
+    return 0;
+}
+
+//void switch_2272_on(struct device *dev) {
+struct packet switch_2272_on(char system_code, char unit_code) {
+    printf("switch on\n");
+    struct packet pkg = {
+        .duration = 100,
+        .count = 1,
+        .data = "101",
+    };
+    return pkg;
+}
+
+struct packet switch_2272_off(char system_code, char unit_code) {
+    printf("switch off\n");
+    struct packet pkg;
+    return pkg;
+}
+
+int main(void)
+{
+    //struct packet foo = {
+    //    .duration = 5,
+    //    .count = 10,
+    //    .data = "01000000101010101010000001010101010107100001",
+    //};
+
+    //struct device *ptr;
+    //ptr = malloc(sizeof(device) * 2);
+    //if(!ptr)
+    //    return NULL;
+    struct device dev_arr[2];
+    struct device tmp;
+
+    tmp.type = SOCKET_TYPE_2272;
+    tmp.id = "ROOM_1__LAMP_1";
+    tmp.category = "living room";
+    tmp.name = "ceiling light";
+    tmp.on = &switch_2272_on,
+    tmp.off = &switch_2272_off,
+    dev_arr[0] = tmp;
+
+    //tmp.type = SOCKET_TYPE_2272;
+    //tmp.id = "ROOM_1__LAMP_2";
+    //tmp.category = "living room";
+    //tmp.name = "desktop";
+    ////tmp.on_function_t = *2272_on,
+    ////tmp.off_function_t = *2272_off,
+    //dev_arr[1] = tmp;
+
+    printf("test: %p\n", dev_arr[0].on);
+    //printf("test: %s\n", dev_arr[1].name);
+
+    struct packet pkg = (dev_arr[0].on)(0x00, 0x01);
+
+    return write(&pkg);
 }
