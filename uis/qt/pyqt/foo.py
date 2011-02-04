437c7f074d9c1203ad276967913420d6405a7c07
Mirko Vogt <dev@nanl.de>
2011-02-04 03:38:27 +0100

    add very simple but working client based on pyqt4 (python + qt4)

---
+++ uis/qt/pyqt/foo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from sys import argv
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+from xmlrpclib import ServerProxy
+
+class GUI(QtGui.QWidget):
+    def __init__(self):
+        super(GUI, self).__init__()
+        self.xmlrpc = None
+        self.initUI()
+
+
+    def initUI(self):
+        self.setWindowTitle('rfm12')
+        self.button = QtGui.QPushButton('Set Host', self)
+        self.button.move(20, 20)
+        self.connect(self.button, QtCore.SIGNAL('clicked()'), self.showDialog)
+
+
+    def showDialog(self):
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 'Set host:')
+        if not ok:
+            return False
+
+        try:
+            self.xmlrpc = ServerProxy(str(text))
+            self.devs = self.xmlrpc.get_config()
+        except:
+            msg_box = QtGui.QMessageBox(
+            QtGui.QMessageBox.Warning,
+            'Warning',
+            'Fetching data from host failed',
+            QtGui.QMessageBox.Ok,
+            self)
+            msg_box.exec_()
+            return False
+
+        self.button.hide()
+        i = 0
+        for dev in self.devs:
+            print "fetched config for device: %i, %s (%s)" % (i, dev['id'], dev['label'])
+            self.devs[i]['_'] = QtGui.QCheckBox(dev['label'], self)
+            self.devs[i]['_'].setObjectName(str(i))
+            self.devs[i]['_'].move(10, i*20+10)
+            self.connect(self.devs[i]['_'], QtCore.SIGNAL('stateChanged(int)'), self.switch)
+            self.devs[i]['_'].show()
+            i += 1
+
+
+    def switch(self, value):
+        if value:
+            self.xmlrpc.on(
+                self.devs[
+                    int(
+                        self.sender().objectName()
+                    )
+                ]['id']
+            )
+        else:
+            self.xmlrpc.off(
+                self.devs[
+                    int(
+                        self.sender().objectName()
+                    )
+                ]['id']
+            )
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(argv)
+    _ = GUI()
+    _.show()
+    app.exec_()
