9898ac76f29d7130d3249227e4364c9d5f82deec
Mirko Vogt <dev@nanl.de>
2011-02-06 17:43:28 +0100

    commit basic UI client application based on ncurses

---
+++ uis/ncurses/foo.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <ncurses.h>
+#include "../../lib/core.h"
+
+#define WIDTH 20
+#define HEIGHT 10
+
+int n_choices;
+
+void print_menu(WINDOW *menu_win, int highlight);
+
+int main()
+{   WINDOW *menu_win;
+    int highlight = 1;
+    int choice = 0;
+    int c;
+    int ret;
+
+    n_choices = create_objs_by_cfg();
+
+
+    initscr();
+    clear();
+    noecho();
+    cbreak();   /* Line buffering disabled - pass on everything */
+
+    menu_win = newwin(HEIGHT, WIDTH, 2, 0);
+    keypad(menu_win, TRUE);
+    mvprintw(0, 0, "Use arrow keys to go up and down,\npress enter to switch socket");
+    refresh();
+    print_menu(menu_win, highlight);
+    while(1)
+    {
+        c = wgetch(menu_win);
+        switch(c)
+        {   case KEY_UP:
+                if(highlight == 1)
+                    highlight = n_choices;
+                else
+                    --highlight;
+                break;
+            case KEY_DOWN:
+                if(highlight == n_choices)
+                    highlight = 1;
+                else 
+                    ++highlight;
+                break;
+            case 10:
+                choice = highlight;
+                break;
+        }
+        print_menu(menu_win, highlight);
+        if(c != 10)
+            continue;
+        ret = control(&dev_arr[choice-1], !dev_arr[choice-1].state);
+        mvprintw(HEIGHT+2, 0, "switched: '%s' to '%i'\nreturn code: %i", dev_arr[choice-1].label, dev_arr[choice-1].state, ret);
+        clrtoeol();
+        refresh();
+        print_menu(menu_win, highlight);
+    }
+}
+
+void print_menu(WINDOW *menu_win, int highlight)
+{
+    int x, y, i;
+
+    x = 2;
+    y = 2;
+    box(menu_win, 0, 0);
+    for(i = 0; i < n_choices; ++i)
+    {   if(highlight == i + 1)
+        {   wattron(menu_win, A_REVERSE); 
+            mvwprintw(menu_win, y, x, "[%i]  %s", dev_arr[i].state, dev_arr[i].label);
+            wattroff(menu_win, A_REVERSE);
+        }
+        else
+            mvwprintw(menu_win, y, x, "[%i]  %s", dev_arr[i].state, dev_arr[i].label);
+        ++y;
+    }
+    wrefresh(menu_win);
+}
